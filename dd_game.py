9b2f9ef6853cdbee93fe3e505478ade58fc22abb
Glenn Velupillai <glennselwynvel@gmail.com>
2018-05-07 23:53:47 -0400

    doctest for get moves

--- dd_game.py
+++ dd_game.py
@@ -52,8 +52,13 @@ def get_locations(cells):
 
 
 def get_moves(player):
-    ''' Show and return the available moves for the player '''
+    """ Based on the tuple of the player's position, return the list of acceptable moves 
     
+    >>>GAME_DIMENSIONS = (2, 2)
+    >>>get_moves((0, 2))
+    ['RIGHT', 'UP', 'DOWN']
+    
+    """
     x, y = player
     moves = ['LEFT', 'RIGHT', 'UP', 'DOWN']
     if x == 0:
